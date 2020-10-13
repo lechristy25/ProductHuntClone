85a70d4be5a792d17aeab636cff459d1e3893b41
Lenoy Christy <lenoy.christy@mail.mcgill.ca>
2020-10-13 12:40:00 -0400

    added firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAOnI_ccrJEPhcwcRAnpXtrpbL7lAlxLjE",
+  authDomain: "hunt-dd92e.firebaseapp.com",
+  databaseURL: "https://hunt-dd92e.firebaseio.com",
+  projectId: "hunt-dd92e",
+  storageBucket: "hunt-dd92e.appspot.com",
+  messagingSenderId: "346999353656",
+  appId: "1:346999353656:web:333c39a4178f1d797f51cd"
+};
+
+export default firebaseConfig;
